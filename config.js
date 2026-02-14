86b23cebebbba66c8925420a7764a632f05b8930
VarunSC258 <139049544+VarunSC258@users.noreply.github.com>
2026-02-14 21:04:14 +0530

    Update config.jsSaloni

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
+    // Example: "Saloni", "Sarah", "Mike"
     valentineName: "Saloni",
 
     // The title that appears in the browser tab
